93d2e9dbbc95edeed4a0f1d677e6832eb9b575cb
林兴栋 <a694701@qq.com>
2018-07-18 09:22:06 +0800

    初始化

---
+++ ZhiBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZhiBook
+//
+//  Created by 林兴栋 on 2018/7/18.
+//  Copyright © 2018年 林兴栋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
